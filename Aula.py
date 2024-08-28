8482eb171d084d8b8d5e3177a30c9f09f364a484
Shaco23 <af553345@gmail.com>
2024-08-28 20:21:28 -0300

    Aula.py

---
+++ Aula.py
@@ -0,0 +1,20 @@
+# AulaPython
+
+a = 2
+b = 6
+c = 3
+
+operacaoAnd = a == b and c * a == b 
+print(operacaoAnd)
+
+operacaoOr = a == b or c * a == b 
+print(operacaoOr)
+
+operacaoAnd = a == c + b and b == c
+print(operacaoAnd) 
+
+operacaoOr = a == c + b or b == c
+print(operacaoOr)
+
+operacaoAnd a > 5 and b > 3
+print(operacaoAnd)
