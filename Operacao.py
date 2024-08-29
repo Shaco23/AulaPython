bf144cad22ebbd5df2ac8b07af1e060f9220eaa0
Shaco23 <af553345@gmail.com>
2024-08-28 21:43:49 -0300

    incluindo operadores aritmeticos

---
+++ Operacao.py
@@ -0,0 +1,38 @@
+a = 52
+b = 85
+c = 151
+
+
+
+
+#Adicao +
+resultado = a + b 
+print(f"soma: {resultado}")
+
+#Subtracao 
+resultado = c - b
+print(f"Subtracao: {resultado}")
+
+#multiplicacao
+resultado = c * a 
+print(f'multiplicacao:{resultado}')
+
+#divisao
+resultado = a / b 
+print(f'divisao:{resultado}')
+
+#divisao inteiro 
+resultado = a // c
+print(f"divisao inteiro:{resultado}")
+
+#resto de divisao
+resultado = a % c 
+print(f"resto de divisao:{resultado}")
+
+#exponencial
+resultado = a ** b
+print(f"exponencial: {resultado}")
+
+
+
+
